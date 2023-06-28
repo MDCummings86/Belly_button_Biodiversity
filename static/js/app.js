bec90f6a48bcb9b97542949d767cd08b8f0d4d87
MDCummings86 <mdcummings@michaels-mbp.lan>
2023-06-27 22:57:05 -0400

    non working code for bubble and bar charts

--- static/js/app.js
+++ static/js/app.js
@@ -53,4 +53,100 @@ d3.selectAll("#selDataset").on("change", getData);
         createBubbleChart(sample_one);
     });
     });
+// Create a function to create the bar chart
+ function createBarChart(sample) {
 
+    // Retrieve all sample data
+    let sampleInfo = data.samples;
+    
+    // Filter based on the value of the sample
+    let value = sampleInfo.filter(result => result.id == sample);
+
+    // Grab the first index from the array
+    let valueData = value[0];
+     
+         // Use D3 to reassign the url as data
+         d3.json(url).then((data) => {
+    
+            // Declare the otu_ids, lables, and sample values
+            let otu_ids = valueData.otu_ids;
+            let otu_labels = valueData.otu_labels;
+            let sample_values = valueData.sample_values;
+    
+            // Log the data to the inspector console
+            console.log(otu_ids,otu_labels,sample_values);
+    
+            // Declare top ten items to display in descending order
+            let yaxis = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
+            let xaxis = sample_values.slice(0,10).reverse();
+            let labels = otu_labels.slice(0,10).reverse();
+            
+            // Set up the trace for the bar chart with horizontal orientation
+            let trace = {
+                x: xaxis,
+                y: yaxis,
+                labels: labels,
+                orientation: "h",
+                type: "bar",
+            };
+    
+            // Setup the layout
+            let layout = {
+                title: "Top 10 OTUs in this Bellybutton"
+            };
+    
+            // Call Plotly to plot the bar chart
+            Plotly.newPlot("bar", [trace], layout)
+        });
+    };
+    
+    // Function that builds the bubble chart
+    function createBubbleChart(sample) {
+    
+        // Use D3 to retrieve all of the data
+        d3.json(url).then((data) => {
+            
+            // Retrieve all sample data
+            let sampleInfo = data.samples;
+    
+            // Filter based on the value of the sample
+            let value = sampleInfo.filter(result => result.id == sample);
+    
+            // Grab the first index from the array
+            let valueData = value[0];
+    
+            // Get otu_ids, otu_lables, and sample_values from the json data
+            let otu_ids = valueData.otu_ids;
+            let otu_labels = valueData.otu_labels;
+            let sample_values = valueData.sample_values;
+    
+            // Log the data in the inspector console
+            console.log(otu_ids,otu_labels,sample_values);
+            
+            // Set up the trace for bubble chart
+            let trace1 = {
+                x: otu_ids,
+                y: sample_values,
+                labels: otu_labels,
+                mode: "markers",
+                marker: {
+                    size: sample_values,
+                    color: otu_ids,
+                    colorscale: "Earth"
+                }
+            };
+    
+            // Set up the layout
+            let layout = {
+                title: "Bacteria Per Sample",
+                hovermode: "closest",
+                xaxis: {title: "OTU ID"},
+            };
+    
+            // Call Plotly to plot the bubble chart
+            Plotly.newPlot("bubble", [trace1], layout)
+        });
+    };
+    
+    // Call the initialize function
+    init()};
